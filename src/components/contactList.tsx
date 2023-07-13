38775f4593f23471a9ad753e2c22dba9a4eec900
ss749236 <ss749236@gmail.com>
2023-07-13 19:13:53 +0530

    side-bar

---
+++ src/components/contactList.tsx
@@ -0,0 +1,12 @@
+const ContactList = () => {
+    return (
+        <div className="container1">
+            <div className="profileInfo">
+                <img className="profileImage" src="https://tse4.mm.bing.net/th?id=OIP.37mLTapohqg7soL2wzLFyQAAAA&pid=Api&P=0&h=180" alt="" />
+            </div>
+
+
+        </div>
+    )
+}
+export default ContactList;
---
+++ src/components/conversationComponent.tsx
@@ -0,0 +1,9 @@
+const ConversationComponent = () => {
+    return (
+        <div className="container2">
+            'conversation'
+        </div>
+
+    )
+}
+export default ConversationComponent;
